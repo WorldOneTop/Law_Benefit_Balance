ce3fe749bfbcd2b84aa74fb72ddb4d5046d257ad
dlwpdlf147 <dlwpdlf147@naver.com>
2022-10-14 19:44:45 +0900

    setting package

--- app/src/main/java/com/worldonetop/lawbenefitbalance/view/activity/MainActivity.kt
+++ app/src/main/java/com/worldonetop/lawbenefitbalance/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.worldonetop.lawbenefitbalance
+package com.worldonetop.lawbenefitbalance.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.worldonetop.lawbenefitbalance.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
